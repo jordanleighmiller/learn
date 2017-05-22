bb7dc6347d495ca762c14fcb6841db0c29241d70
jordanleighmiller <jordanmiller595@gmail.com>
2017-05-21 20:15:53 -0500

    Its alive

---
+++ test.py
@@ -0,0 +1,8 @@
+Youre_a_bad_habit = int(input("Some Number"))
+
+if(Youre_a_bad_habit>=3):
+	print("Your mother was a hamster")
+elif(Youre_a_bad_habit==2):
+	print("Your number is %d" % Youre_a_bad_habit)
+else:
+	print("What ever you want")
