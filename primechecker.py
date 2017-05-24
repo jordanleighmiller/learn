10edd19ed8bc9c0ebf753bca16b07ae468caeb72
binarysaurus <im@joe.cloud>
2017-05-23 21:49:40 -0500

    Added prime checker

---
+++ primechecker.py
@@ -0,0 +1,21 @@
+# FUNCTION: checkprime
+# ARGS: number = number to check if prime
+# RETURNS:
+# true: if number is prime
+# false: if number is NOT prime
+def checkprime(number):
+    for i in range(2, number):
+        if (number % i == 0):
+            return False
+    return True
+
+N = 0
+
+while N != -1:
+
+    N = int(input("Input a number: "))
+
+    if checkprime(abs(N)):
+        print ("%d is prime." % N)
+    else:
+        print("%d is NOT prime." % N)
